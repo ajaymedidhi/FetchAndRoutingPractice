e32275eb2f36f696c3cc4f9672452c2c9943c2f0
Ajay Medidhi <ajaymedidhi858.com>
2023-09-20 06:59:20 +0000

    added fetch

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,54 @@
+import {Component} from 'react'
+
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+class BlogList extends Component {
+  state = {isLoading: true, blogList: []}
+
+  componentDidMount() {
+    this.getBlogDate()
+  }
+
+  getBlogDate = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    const formattedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+      author: eachItem.author,
+      topic: eachItem.topic,
+    }))
+
+    this.setState({blogList: formattedData, isLoading: false})
+  }
+
+  render() {
+    const {blogList, isLoading} = this.state
+
+    return (
+      <div className="blog-list-container">
+        {isLoading ? (
+          <Loader
+            data-testid="loader"
+            type="TailSpin"
+            color="#00BFFF"
+            height={50}
+            width={50}
+          />
+        ) : (
+          blogList.map(item => <BlogItem blogData={item} key={item.id} />)
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogList
---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+import UserInfo from '../UserInfo'
+
+import BlogList from '../BlogList'
+
+const Home = () => (
+  <div className="home-container">
+    <h1>title</h1>
+    <UserInfo />
+
+    <BlogList />
+  </div>
+)
+
+export default Home
